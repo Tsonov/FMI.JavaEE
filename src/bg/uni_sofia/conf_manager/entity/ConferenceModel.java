68278dab7a0ff2cc52061565dc7c5f67d643324a
Nia Doncheva <niadonceva@abv.bg>
2015-07-02 02:44:47 +0300

    Revert "Revert "Can't add lectures if conference is sooner than 1 month""
    This reverts commit cd2ff37c9f83619817e784968e96eef5f24f35c3.

--- src/bg/uni_sofia/conf_manager/entity/ConferenceModel.java
+++ src/bg/uni_sofia/conf_manager/entity/ConferenceModel.java
@@ -1,6 +1,7 @@
 package bg.uni_sofia.conf_manager.entity;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -9,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import bg.uni_sofia.conf_manager.utils.DateUtils;
 
 @Entity
 @Table(name = "CONFERENCES")
@@ -56,7 +59,7 @@ public class ConferenceModel implements Serializable {
 		this.venueAddress = venueAddress;
 	}
 
-	@Column(name="start_date")
+	@Column(name="start_date", nullable=false)
 	public Date getStartDate() {
 		return startDate;
 	}
@@ -65,7 +68,7 @@ public class ConferenceModel implements Serializable {
 		this.startDate = startDate;
 	}
 
-	@Column(name="end_date")
+	@Column(name="end_date", nullable=false)
 	public Date getEndDate() {
 		return endDate;
 	}
@@ -74,21 +77,10 @@ public class ConferenceModel implements Serializable {
 		this.endDate = endDate;
 	}
 	
-//	@OneToMany(mappedBy="conference", fetch=FetchType.EAGER)
-//	public List<LectureModel> getLectures() {
-//		return lectures;
-//	}
-//
-//	public void setLectures(List<LectureModel> lectures) {
-//		this.lectures = lectures;
-//	}
-//	
-//	public void addLecture(LectureModel lecture) {
-//		this.getLectures().add(lecture);
-//		if(lecture.getConference() != this) {
-//			lecture.setConference(this);
-//		}
-//	}
+	public boolean canAddLectures() {
+		int months = DateUtils.getMonthsBetweenTodayAnd(getStartDate());
+		return months >= 1;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
